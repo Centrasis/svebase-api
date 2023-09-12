c7794447635c64e702fb717e9b059f4fa7606e79
Felix Lehner <f.lehner@tu-bs.de>
2023-09-12 20:17:33 +0200

    init

--- src/service_info.ts
+++ src/service_info.ts
@@ -13,7 +13,7 @@ export interface StorageIntegrity {
 
 export interface Status {
     status: boolean;
-    verstion: string;
+    version: string;
     storage_integrity: StorageIntegrity;
     authorization_host: string;
 }
@@ -30,11 +30,23 @@ export class ServiceInfo {
     public static auth_api_host: string = (process.env.AUTH_API_HOST) ? process.env.AUTH_API : "authentication.localhost";
     public static status_provider: StatusProvider | undefined;
     public static get_status(): Status {
-        return {
-            status: ServiceInfo.status_provider.status(),
-            verstion: ServiceInfo.status_provider.version(),
-            storage_integrity: ServiceInfo.status_provider.storage_integrity(),
-            authorization_host: ServiceInfo.status_provider.authorization_host()
+        if (ServiceInfo.status_provider !== undefined) {
+            return {
+                status: ServiceInfo.status_provider.status(),
+                version: ServiceInfo.status_provider.version(),
+                storage_integrity: ServiceInfo.status_provider.storage_integrity(),
+                authorization_host: ServiceInfo.status_provider.authorization_host()
+            }
+        } else {
+            return {
+                status: false,
+                authorization_host: "Unknown",
+                storage_integrity: {
+                    persistent: false,
+                    volatile: false
+                },
+                version: "Unknown"
+            }
         }
     }
 }
